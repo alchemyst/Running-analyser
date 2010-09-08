2212d6adcf89f61ad6234b510f04df9620672a2e
Carl Sandrock <carl.sandrock@gmail.com>
2010-09-08 07:10:47 +0200

    Abstracted table handling for report in bestoverdistance to tabulate.py

--- bestoverdistance.py
+++ bestoverdistance.py
@@ -4,12 +4,24 @@ import numpy
 import scipy.stats
 import csv
 import progressbar
+import tabulate
 
 def findbesttime(t, d, targets):
+    """Find best times over target distances
+
+    Given a history of times and distances, figure out what the best
+    times where for those distances with a sliding window
+    interpolation.
+    """
+    # Start out thinking all are infinite
     bests = numpy.inf*targets
+    # Loop through the time data
     for i in range(len(t)-1):
-        thesetimes = numpy.interp(targets, d[i+1:]-d[i], t[i+1:]-t[i],
+        # Interpolate the times for the target distances, as though
+        # starting from this time step
+        thesetimes = numpy.interp(targets, d[i+1:]-d[i], t[i+1:]-t[i], 
                                   right=numpy.inf)
+        # Update bests
         bests = numpy.minimum(thesetimes, bests)
     return bests
 
@@ -20,8 +32,29 @@ def writebests(filename, distances, bests):
             print >> ofile, d, b
     print >> ofile
 
+def divparts(value, parts):
+    """ Calculate a split of value into smaller units or parts.
+    
+    For instance, there are 60 seconds in a minute, 60 minutes in an hours and 24 hours in a day, so to split a number of seconds to seconds, minutes, hours, 
+    days would be: 
+    
+    >> divparts(60*60*24+1, (60, 60, 24))
+    [1, 0, 1]
+    >> divparts(1234, (10, 10, 10))
+    [4, 3, 2, 1]
+    """
+    retval = []
+    for part in parts:
+        [value, wholeparts] = divmod(value, part)
+        retval.append(wholeparts)
+    retval.append(value)
+    return retval        
+
 def hms(secs):
-    """ Calculate hours minutes and seconds from seconds """
+    """ Calculate hours minutes and seconds from seconds
+
+    Obsolete: this should be replaced by divparts
+    """
     m, s = divmod(secs, 60)
     h, m = divmod(m, 60)
     return (h, m, s)
@@ -36,22 +69,22 @@ distances = distances[distances <= maxdistance]
 # process
 bests = numpy.inf*distances
 runset = set(rundata["track"])
-widgets = ['Analysing: ',
-           progressbar.Percentage(),
+widgets = ['Analysing: ', 
+           progressbar.Percentage(), 
            ' ', progressbar.Bar("="), ' ', progressbar.ETA()]
 pbar = progressbar.ProgressBar(widgets=widgets, maxval=max(runset)).start()
 
 besthistory = numpy.zeros([len(runset), len(distances)])
 for tracki, track in enumerate(sorted(runset)):
     trackdata = rundata[rundata['track']==track]
-    trackbests = findbesttime(trackdata['seconds'], trackdata['distance'],
+    trackbests = findbesttime(trackdata['seconds'], trackdata['distance'], 
                               targets=distances)
     bests = numpy.minimum(trackbests, bests)
     besthistory[tracki, :] = bests
     writebests('bests/upto_%04i.dat'%track, distances, bests)
     writebests('bests/track_%04i.dat'%track, distances, trackbests)
     pbar.update(track)
-besth = file('besthistory.dat', 'w')
+
 numpy.savetxt('besthistory.dat', besthistory.T)
 writebests('bestoverdistance.dat', distances, bests)
 pbar.finish()
@@ -59,21 +92,39 @@ pbar.finish()
 # Figure out how we are doing compared to world records
 records = numpy.loadtxt('records.dat')
 # interpolate the watchdistances in the records file
-watchrecords = numpy.interp(distances, records[:,0], records[:,1])
+watchrecords = numpy.interp(distances, records[:, 0], records[:, 1])
 # geometric mean better for fractions
 fworldrecords = scipy.stats.gmean(watchrecords/bests)
 flasttobest = 1/scipy.stats.gmean(filter(numpy.isfinite, trackbests/bests))
 print "We are doing %2.1f%% of world records" % (fworldrecords*100)
 print "Last run was %2.1f%% of best" % (flasttobest*100)
 
-# show where we did well
-newbests = numpy.arange(len(bests))[bests == trackbests]
-if len(newbests) > 0:
-    print "New bests:"
-    for track in newbests:
-        h, m, s = hms(bests[track])
-        ph, pm, ps = hms(bests[track]/distances[track]*1000.0)
-        print "%2.1fm\t%ih%02i:%02i\t%2i:%02i min/km" % (distances[track], h, m, s, pm, ps)
+# show how we did:
+
+def form(row):
+    distance, lasttime, besttime = row
+    if distance<1000:
+        dstr = "%i m" % distance
+    else:
+        dstr = "%.2f km" % (distance/1000)
+    s, m , h = divparts(lasttime, (60, 60))
+    if h > 0:
+        time = "%ih%02i:%02i" % (h, m, s)
+    else:
+        time = "%02i:%02i" % (m, s)
+    speed = "%.2f" % ((distance/1000.0)/(lasttime/(60.0*60.0)))
+    ps, pm = divparts(lasttime/distance*1000.0, (60,))
+    pace = "%2i:%02i" % (pm, ps)
+    if lasttime == besttime:
+        star = "*"
+    else:
+        star = ""
+    return dstr, time, speed, pace, star
+
+good = ~numpy.isinf(trackbests)
+tabulate.printtable(headers=["Distance", "Time", "Speed", "Pace", ""],
+                    data=zip(distances[good], trackbests[good], bests[good]),
+                    rowformatter=form)
 
 # Add results for last run
 distances = list(distances)
---
+++ tabulate.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# generalised tabular output
+
+
+def printtable(headers, data, formats=None, rowformatter=None):
+    assert formats is None or rowformatter is None,\
+           "formats or rowformatter must be given"
+    
+    # For now, assume headers
+    collengths = map(len, headers)
+    # for now, assume all rows have same # of cols
+    alldata = list(data)
+    outputrows = []
+    for row in alldata:
+        # Do the transforms on this row
+        if formats:
+            transformedrow = [f(i) for f, i in zip(formats, row)]
+        elif rowformatter:
+            transformedrow = rowformatter(row)
+        # figure out how long each of the strings are
+        lengths = map(len, transformedrow)
+        collengths = map(max, zip(collengths, lengths))
+        outputrows.append(transformedrow)
+    outputformats = ["%%%is" % i for i in collengths]
+
+    def rowstring(items):
+        return " ".join(f % i for f, i in zip(outputformats, items))
+
+    print rowstring(headers)
+    print '-'*(sum(collengths) + len(collengths))
+    for row in outputrows:
+        print rowstring(row)
+
+doubleformatter = lambda(x): "%5.3f (double)" % x
+
+if __name__ == "__main__":
+    printtable(headers=['this', 'is', 'a'],
+               data=[[1.2, 'of', 'the'],
+                     [4e-6, 50, 'Formatter']],
+               formats=[doubleformatter, str, str])
+        
